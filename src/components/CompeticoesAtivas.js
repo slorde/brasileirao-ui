b9ba793e42225c4f5fe85d009795da921d0a4358
Gabriel <slorde2@gmail.com>
2021-05-27 20:40:34 -0300

    hall

--- src/components/CompeticoesAtivas.js
+++ src/components/CompeticoesAtivas.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Button, Spinner } from 'react-bootstrap';
+import { Form, Button, Spinner, ListGroup, Table } from 'react-bootstrap';
 import { reactLocalStorage } from 'reactjs-localstorage';
 import axios from 'axios';
 import ResultadoCompeticao from './ResultadoCompeticao'
@@ -9,7 +9,7 @@ import {withRouter} from 'react-router-dom';
 class CompeticoesAtivas extends Component {
     constructor(props) {
         super();
-        this.state = { competicoes: [], isFetching: false }
+        this.state = { competicoes: [], isFetching: false, dadosBobos: [] }
         const { REACT_APP_BRASILEIRO_API } = process.env;
         this.host = REACT_APP_BRASILEIRO_API;
     }
@@ -26,6 +26,16 @@ class CompeticoesAtivas extends Component {
                 this.setState({ isFetching: false});
               console.log(error);
             });
+
+            axios.get(`${this.host}/competicoes/bobos`, { headers: { Authorization: auth }})
+            .then((response) => {
+              this.setState({ dadosBobos: response.data })
+            })
+            .catch((error) => {
+                this.setState({ isFetching: false});
+              console.log(error);
+            });
+
         } catch(error) {
             console.log(error);
         }     
@@ -50,12 +60,44 @@ class CompeticoesAtivas extends Component {
                 </div>
         });
 
+        const campeoes = this.state.dadosBobos
+            .sort(dados => dados.qtdCampeao)
+            .reverse()
+            .map(campeao => {
+            
+                const boboIndex = this.state.dadosBobos.findIndex(p => p.nome === campeao.nome);
+                const theme =  boboIndex === 0 ? 'warning' : (boboIndex === 3 ? 'danger' : 'dark');
+                
+                return  <ListGroup key={campeao.nome} horizontal>
+                    <ListGroup.Item className="HallNome" key={campeao.nome} variant={theme}>{campeao.nome}</ListGroup.Item>
+                    <ListGroup.Item className="HallCampeonatos" key={campeao.qtdCampeao} variant={theme}>{campeao.qtdCampeao}</ListGroup.Item>
+                    <ListGroup.Item className="HallPontuacao" key={campeao.pontuacao} variant={theme}>{campeao.pontuacao}</ListGroup.Item>
+                </ListGroup>
+        });
+        
+        const campeoesHeader = <ListGroup key={'header'} horizontal>
+        <ListGroup.Item className="HallNome" key='headernome' variant='success'>Nome</ListGroup.Item>
+        <ListGroup.Item className="HallCampeonatos" key='headercampeao' variant='success'>campeonatos</ListGroup.Item>
+        <ListGroup.Item className="HallPontuacao" key='headerpontuacao' variant='success'>pontuação total</ListGroup.Item>
+    </ListGroup>
 
         return <div key="compativa" className="Comp">
-            <p>Em andamento</p>
-            <Form>
-                {this.state.isFetching ? loading : conteudo}
-            </Form>
+            <Table>
+                <tr>
+                    <td>
+                        <p>Em andamento</p>
+                        <Form>
+                            {this.state.isFetching ? loading : conteudo}
+                        </Form>
+                    </td>
+                    <td>
+                        <Form className="hall">
+                            {campeoes.length ? campeoesHeader: <br/>}
+                            {campeoes}
+                        </Form>
+                    </td>
+                </tr>
+            </Table>
       </div>
     };
 }
